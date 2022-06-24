77f6d448f508b40675fc0dbfcb455ba4ef2d538a
hasan <mdhasanhk.98@gmail.com>
2022-06-24 02:19:16 -0400

    function

---
+++ function.js
@@ -0,0 +1,12 @@
+nums = [29, 15, 10, 4, 5, 12];
+
+for(var i = 0; i < nums.length; i++) {
+    var num = nums[i];
+    console.log(num);
+    if(num % 2 == 0) {
+        console.log("oven")
+    }
+    else{
+        console.log("odd");
+    }
+}
